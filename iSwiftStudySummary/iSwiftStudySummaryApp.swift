fa0b2a64f386351f57cbaa89a59e1f4234fdf0b1
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2022-12-03 10:18:10 +0700

    Initial Commit

---
+++ iSwiftStudySummary/iSwiftStudySummaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  iSwiftStudySummaryApp.swift
+//  iSwiftStudySummary
+//
+//  Created by Ahmad Syarifuddin Randiko on 03/12/22.
+//
+
+import SwiftUI
+
+@main
+struct iSwiftStudySummaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
