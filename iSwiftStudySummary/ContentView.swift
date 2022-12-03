430f6c9a650ff0533bb9644ab79f76705b310fae
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2022-12-03 10:24:08 +0700

    create tab view

--- iSwiftStudySummary/ContentView.swift
+++ iSwiftStudySummary/ContentView.swift
@@ -8,17 +8,68 @@
 import SwiftUI
 
 struct ContentView: View {
+    //MARK: Properties
+    //Variable selectedTab is used for moving between tabs programmatically.
+    @State private var selectedTab: String = "Browse"
+    
+    //MARK: Body
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationStack {
+            TabView(selection: $selectedTab){
+                Text("Browse".uppercased())
+                    .font(.system(.largeTitle, design: .rounded))
+                    .tabItem{
+                        Image(systemName: "square.grid.2x2")
+                        Text("Browse")
+                    }
+                    .tag("Browse")
+                
+                VStack {
+                    Text("Watch".uppercased())
+                        .font(.system(.largeTitle, design: .rounded))
+                    
+                    Button{
+                        selectedTab = "Settings"
+                    }label: {
+                        Text("Show Settings")
+                            .font(.system(.headline, design: .rounded))
+                            .padding()
+                            .foregroundColor(.white)
+                            .background(.indigo)
+                            .cornerRadius(12)
+                        
+                    }
+                    
+                }
+                .tabItem{
+                    Image(systemName: "play.rectangle")
+                    Text("Watch")
+                }
+                .tag("Watch")
+                
+                Text("Loans".uppercased())
+                    .font(.system(.largeTitle, design: .rounded))
+                    .tabItem{
+                        Image(systemName: "rectangle.and.text.magnifyingglass")
+                        Text("Loans")
+                    }
+                    .tag("Loans")
+                
+                Text("Settings".uppercased())
+                    .font(.system(.largeTitle, design: .rounded))
+                    .tabItem{
+                        Image(systemName: "slider.horizontal.3")
+                        Text("Settings")
+                    }
+                    .tag("Settings")
+            } //- TabView
+            .tint(.indigo)
+        } //- NavigationStack
+
     }
 }
 
+//MARK: Preview
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
